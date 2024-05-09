d25ca88d243bea01199b2800c30ba94efa7a507a
Guilherme vinicios conde <guicondee@outlook.com>
2024-05-09 10:38:34 -0300

    add h-screen global style

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header/header";
 
 export function App() {
   return (
-    <div className="flex flex-col gap-2 max-w-7xl mx-auto py-5">
+    <div className="flex flex-col gap-2 max-w-7xl mx-auto py-5 h-screen">
       <Header />
       <AttendeeList />
     </div>
